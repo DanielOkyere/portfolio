5bdbdbdb87ec45a3a4a3eb15356a82bf9805df8d
danielenyata <daniel.kwame.okyere101@gmail.com>
2022-02-13 13:13:10 +0000

    Udated Cv

--- public/script.js
+++ public/script.js
@@ -1,120 +1,126 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
+$(document).ready(function () {
+    $(window).scroll(function () {
         // sticky navbar on scroll script
-        if(this.scrollY > 20){
-            $('.navbar').addClass("sticky");
-        }else{
-            $('.navbar').removeClass("sticky");
+        if (this.scrollY > 20) {
+            $(".navbar").addClass("sticky");
+        } else {
+            $(".navbar").removeClass("sticky");
         }
-        
+
         // scroll-up button show/hide script
-        if(this.scrollY > 500){
-            $('.scroll-up-btn').addClass("show");
-        }else{
-            $('.scroll-up-btn').removeClass("show");
+        if (this.scrollY > 500) {
+            $(".scroll-up-btn").addClass("show");
+        } else {
+            $(".scroll-up-btn").removeClass("show");
         }
     });
 
     // slide-up script
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
+    $(".scroll-up-btn").click(function () {
+        $("html").animate({ scrollTop: 0 });
         // removing smooth scroll on slide-up button click
-        $('html').css("scrollBehavior", "auto");
+        $("html").css("scrollBehavior", "auto");
     });
 
-    $('.navbar .menu li a').click(function(){
+    $(".navbar .menu li a").click(function () {
         // applying again smooth scroll on menu items click
-        $('html').css("scrollBehavior", "smooth");
+        $("html").css("scrollBehavior", "smooth");
     });
 
     // toggle menu/navbar script
-    $('.menu-btn').click(function(){
-        $('.navbar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
+    $(".menu-btn").click(function () {
+        $(".navbar .menu").toggleClass("active");
+        $(".menu-btn i").toggleClass("active");
     });
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["DevOps Engineer",  "Commerce cloud Developer", "MERN Stack Developer", "Freelancer"],
-        typeSpeed: 100,
+        strings: [
+            "Commerce cloud Developer",
+            "MEAN Stack Developer",
+            "Freelancer",
+        ],
+        typeSpeed: 80,
         backSpeed: 60,
-        loop: true
+        loop: true,
     });
 
     var typed = new Typed(".typing-2", {
-        strings: [ " Commerce cloud Developer", "DevOps Engineer", "MERN Stack Developer", "Freelancer"],
-        typeSpeed: 100,
+        strings: [
+            " Commerce cloud Developer",
+            "MEAN Stack Developer",
+            "Freelancer",
+        ],
+        typeSpeed: 80,
         backSpeed: 60,
-        loop: true
+        loop: true,
     });
 
     // owl carousel script
-    $('.carousel').owlCarousel({
+    $(".carousel").owlCarousel({
         margin: 20,
         loop: false,
         autoplayTimeOut: 2000,
         autoplayHoverPause: true,
         responsive: {
-            0:{
+            0: {
                 items: 1,
-                nav: false
+                nav: false,
             },
-            600:{
+            600: {
                 items: 2,
-                nav: false
+                nav: false,
             },
-            1000:{
+            1000: {
                 items: 3,
-                nav: false
-            }
-        }
+                nav: false,
+            },
+        },
     });
-
 
     window.addEventListener("DOMContentLoaded", function () {
         // get the form elements defined in your form HTML above
-      
+
         var form = document.getElementById("contactForm");
         // var button = document.getElementById("my-form-button");
         var status = document.getElementById("status");
-      
+
         // Success and Error functions for after the form is submitted
-      
+
         function success() {
-          form.reset();
-          status.classList.add("success");
-          status.innerHTML = "Thanks!";
+            form.reset();
+            status.classList.add("success");
+            status.innerHTML = "Thanks!";
         }
-      
+
         function error() {
-          status.classList.add("error");
-          status.innerHTML = "Oops! There was a problem.";
+            status.classList.add("error");
+            status.innerHTML = "Oops! There was a problem.";
         }
-      
+
         // handle the form submission event
-      
+
         form.addEventListener("submit", function (ev) {
-          ev.preventDefault();
-          var data = new FormData(form);
-          ajax(form.method, form.action, data, success, error);
+            ev.preventDefault();
+            var data = new FormData(form);
+            ajax(form.method, form.action, data, success, error);
         });
-      });
-      
-      // helper function for sending an AJAX request
-      
-      function ajax(method, url, data, success, error) {
+    });
+
+    // helper function for sending an AJAX request
+
+    function ajax(method, url, data, success, error) {
         var xhr = new XMLHttpRequest();
         xhr.open(method, url);
         xhr.setRequestHeader("Accept", "application/json");
         xhr.onreadystatechange = function () {
-          if (xhr.readyState !== XMLHttpRequest.DONE) return;
-          if (xhr.status === 200) {
-            success(xhr.response, xhr.responseType);
-          } else {
-            error(xhr.status, xhr.response, xhr.responseType);
-          }
+            if (xhr.readyState !== XMLHttpRequest.DONE) return;
+            if (xhr.status === 200) {
+                success(xhr.response, xhr.responseType);
+            } else {
+                error(xhr.status, xhr.response, xhr.responseType);
+            }
         };
         xhr.send(data);
-      }
-
+    }
 });
